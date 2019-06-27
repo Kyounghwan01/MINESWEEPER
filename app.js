860765ea0f00be97586332fd97c964fc25a813bc
kyounghwan Noh <44187477+Kyounghwan01@users.noreply.github.com>
2019-06-27 16:34:46 +0900

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ let isReady = false;
 let hor;
 let timerFun;
 let mine;
+let mineC;
 let difficulty;
-let 열은칸 = 0; //openblock
+let 열은칸 = 0;
 let 코드표 = {
   연칸: -1,
   물음표: -2,
@@ -51,9 +52,6 @@ function timer() {
 easyBtn.addEventListener("click", function() {
   refreshBtn.classList.remove("regame");
   difficulty = 1;
-  hor = 10;
-  mine = 20;
-  mineCounter.textContent = mine;
   del();
   timer();
   tableMaker("easy");
@@ -61,9 +59,6 @@ easyBtn.addEventListener("click", function() {
 normalBtn.addEventListener("click", function() {
   refreshBtn.classList.remove("regame");
   difficulty = 2;
-  hor = 15;
-  mine = 30;
-  mineCounter.textContent = mine;
   del();
   timer();
   tableMaker("normal");
@@ -72,9 +67,6 @@ normalBtn.addEventListener("click", function() {
 hardBtn.addEventListener("click", function() {
   refreshBtn.classList.remove("regame");
   difficulty = 3;
-  hor = 20;
-  mine = 40;
-  mineCounter.textContent = mine;
   del();
   timer();
   tableMaker("hard");
@@ -95,12 +87,24 @@ refreshBtn.addEventListener("click", function() {
   timer();
 });
 
-function tableMaker() {
+function tableMaker(text) {
+  열은칸 = 0;
+  if (text === "easy") {
+    hor = 10;
+    mine = 20;
+  } else if (text === "normal") {
+    hor = 15;
+    mine = 30;
+  } else {
+    hor = 20;
+    mine = 40;
+  }
+  mineC = mine;
+  mineCounter.textContent = mineC;
   tbody.innerHTML = "";
   dataset = [];
   isReady = false;
 
-  //지뢰 위치
   let mineList = Array(hor * hor)
     .fill()
     .map((c, i) => i);
@@ -113,16 +117,15 @@ function tableMaker() {
     )[0];
     ranMineList.push(mineNumber);
   }
-
-  //지뢰 테이블
+  
   for (let i = 0; i < hor; i++) {
     let arr = [];
     let tr = document.createElement("tr");
     dataset.push(arr);
     for (let j = 0; j < hor; j++) {
       arr.push(코드표.보통칸);
-      let td = document.createElement("td")
-      td.classList.add('chance');
+      let td = document.createElement("td");
+      td.classList.add("chance");
       td.addEventListener("contextmenu", rightClick);
 
       td.addEventListener("click", function(e) {
@@ -148,66 +151,81 @@ function tableMaker() {
         ) {
           return;
         }
-        //클릭하면
-        // e.currentTarget.classList.add("opened");
         열은칸 += 1;
         if (dataset[blankCol][blank] === 코드표.지뢰) {
-          // 지뢰 클릭
+          let mineAll = document.querySelectorAll(".MINE");
+          for (let i = 0; i < mineAll.length; i++) {
+            mineAll[i].innerHTML =
+              "<img src='https://minesweeper.online/img/skins/hd/mine.svg' width='25px' height='25px' />";
+          }
           e.currentTarget.innerHTML =
             "<img src='https://minesweeper.online/img/skins/hd/mine_red.svg' width='25px' height='25px' />";
-          clearInterval(timerFun);
           isReady = true;
+          clearInterval(timerFun);
           refreshBtn.classList.add("regame");
-        }else{ //지뢰 아닐때 주위의 지뢰 개수 표현
+        } else {clearInterval(timerFun);
           var 주변 = [
-            dataset[blankCol][blank-1], dataset[blankCol][blank+1],
+            dataset[blankCol][blank - 1],
+            dataset[blankCol][blank + 1]
           ];
-          if (dataset[blankCol-1]) {
-            주변 = 주변.concat([dataset[blankCol-1][blank-1], dataset[blankCol-1][blank], dataset[blankCol-1][blank+1]]);
+          if (dataset[blankCol - 1]) {
+            주변 = 주변.concat([
+              dataset[blankCol - 1][blank - 1],
+              dataset[blankCol - 1][blank],
+              dataset[blankCol - 1][blank + 1]
+            ]);
           }
-          if (dataset[blankCol+1]) {
-            주변 = 주변.concat([dataset[blankCol+1][blank-1], dataset[blankCol+1][blank], dataset[blankCol+1][blank+1]]);
+          if (dataset[blankCol + 1]) {
+            주변 = 주변.concat([
+              dataset[blankCol + 1][blank - 1],
+              dataset[blankCol + 1][blank],
+              dataset[blankCol + 1][blank + 1]
+            ]);
           }
           var 주변지뢰개수 = 주변.filter(function(v) {
-            return [코드표.지뢰, 코드표.깃발지뢰, 코드표.물음표지뢰].includes(v);
+            return [코드표.지뢰, 코드표.깃발지뢰, 코드표.물음표지뢰].includes(
+              v
+            );
           }).length;
           e.currentTarget.innerHTML = sourceImgUrl[주변지뢰개수];
-          e.currentTarget.classList.remove('chance');
+          e.currentTarget.classList.remove("chance");
           dataset[blankCol][blank] = 코드표.연칸;
-          if(주변지뢰개수 === 0){
+          if (주변지뢰개수 === 0) {
             var 주변칸 = [];
-            if (tbody.children[blankCol-1]) {
+            if (tbody.children[blankCol - 1]) {
               주변칸 = 주변칸.concat([
                 tbody.children[blankCol - 1].children[blank - 1],
                 tbody.children[blankCol - 1].children[blank],
-                tbody.children[blankCol - 1].children[blank + 1],
+                tbody.children[blankCol - 1].children[blank + 1]
               ]);
             }
             주변칸 = 주변칸.concat([
               tbody.children[blankCol].children[blank - 1],
-              tbody.children[blankCol].children[blank + 1],
+              tbody.children[blankCol].children[blank + 1]
             ]);
 
-            if (tbody.children[blankCol+1]) {
+            if (tbody.children[blankCol + 1]) {
               주변칸 = 주변칸.concat([
                 tbody.children[blankCol + 1].children[blank - 1],
                 tbody.children[blankCol + 1].children[blank],
-                tbody.children[blankCol + 1].children[blank + 1],
+                tbody.children[blankCol + 1].children[blank + 1]
               ]);
             }
-            주변칸.filter(function (v) {
-              return !!v;
-            }).forEach(function(o) {
-              let blank = Array.prototype.indexOf.call(parentTr.children, o);
-              if (dataset[blankCol][blank] !== 코드표.연칸) {
-                o.click();
-              }
-            });
+            주변칸
+              .filter(function(v) {
+                return !!v;
+              })
+              .forEach(function(o) {
+                let blank = Array.prototype.indexOf.call(parentTr.children, o);
+                if (dataset[blankCol][blank] !== 코드표.연칸) {
+                  o.click();
+                }
+              });
           }
         }
-        if (열은칸 === hor * hor - mine) {
+        if (열은칸 === hor * hor - mine && mineC === 0) {
           isReady = true;
-          alert('clear!');
+          alert("clear!");
           clearInterval(timerFun);
           refreshBtn.classList.add("regame");
         }
@@ -217,7 +235,7 @@ function tableMaker() {
         var 부모tbody = e.currentTarget.parentNode.parentNode;
         var 칸 = Array.prototype.indexOf.call(부모tr.children, e.currentTarget);
         var 줄 = Array.prototype.indexOf.call(부모tbody.children, 부모tr);
-        if(dataset[줄][칸] === 코드표.연칸) return;
+        if (dataset[줄][칸] === 코드표.연칸) return;
         td.classList.add("mouseDown");
       });
       td.addEventListener("mouseup", function() {
@@ -229,13 +247,12 @@ function tableMaker() {
     tbody.appendChild(tr);
   }
 
-  //지뢰 심기
   for (let i = 0; i < ranMineList.length; i++) {
     let col = Math.floor(ranMineList[i] / hor);
     let row = ranMineList[i] % hor;
     tbody.children[col].children[row].innerHTML =
-      "<img src='https://minesweeper.online/img/skins/hd/mine.svg' width='25px' height='25px' />";
-    tbody.children[col].children[row].classList.add('MINE');
+      "<img src='https://minesweeper.online/img/skins/hd/closed.svg' width='25px' height='25px' />";
+    tbody.children[col].children[row].classList.add("MINE");
     dataset[col][row] = 코드표.지뢰;
   }
 }
@@ -250,14 +267,14 @@ function rightClick(e) {
   let blank = Array.prototype.indexOf.call(parentTr.children, e.currentTarget);
   let blankCol = Array.prototype.indexOf.call(parentBody.children, parentTr);
   if (
-    e.currentTarget.classList.contains('chance') ||
-    e.currentTarget.classList.contains('MINE')
+    e.currentTarget.classList.contains("chance") ||
+    e.currentTarget.classList.contains("MINE")
   ) {
     e.currentTarget.textContent = ".";
-    mineCounter.textContent = mine -= 1;
+    mineCounter.textContent = mineC -= 1;
     e.currentTarget.classList.add("flag");
-    e.currentTarget.classList.remove('MINE');
-    e.currentTarget.classList.remove('chance')
+    e.currentTarget.classList.remove("MINE");
+    e.currentTarget.classList.remove("chance");
     if (dataset[blankCol][blank] === 코드표.지뢰) {
       dataset[blankCol][blank] = 코드표.깃발지뢰;
     } else {
@@ -266,7 +283,7 @@ function rightClick(e) {
   } else if (e.currentTarget.textContent === ".") {
     e.currentTarget.textContent = "`";
     e.currentTarget.classList.remove("flag");
-    mineCounter.textContent = mine += 1;
+    mineCounter.textContent = mineC += 1;
     e.currentTarget.classList.add("question");
     if (dataset[blankCol][blank] === 코드표.깃발지뢰) {
       dataset[blankCol][blank] = 코드표.물음표지뢰;
@@ -277,13 +294,19 @@ function rightClick(e) {
     e.currentTarget.classList.remove("question");
     if (dataset[blankCol][blank] === 코드표.물음표지뢰) {
       e.currentTarget.innerHTML =
-        "<img src='https://minesweeper.online/img/skins/hd/mine.svg' width='25px' height='25px' />";
-      e.currentTarget.classList.add('MINE');
+        "<img src='https://minesweeper.online/img/skins/hd/closed.svg' width='25px' height='25px' />";
+      e.currentTarget.classList.add("MINE");
       dataset[blankCol][blank] = 코드표.지뢰;
     } else {
       e.currentTarget.textContent = "";
-      e.currentTarget.classList.add('chance');
+      e.currentTarget.classList.add("chance");
       dataset[blankCol][blank] = 코드표.보통칸;
     }
+  }
+  if (열은칸 === hor * hor - mine && mineC === 0) {
+    isReady = true;
+    alert("clear!");
+    clearInterval(timerFun);
+    refreshBtn.classList.add("regame");
   }
 }
